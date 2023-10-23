606173a33be2ca35477e5543136f1206be32dc0e
unknown <true_hearty@nate.com>
2023-10-23 17:25:47 +0900

    20231023

---
+++ Chap24(스레드)/src/kr/co/himedia/thread03/JoinTest.java
@@ -0,0 +1,44 @@
+package kr.co.himedia.thread03;
+
+/*
+ * 1 부터 50,
+ * 51부터 100까지의 합을 구하는 두개의 Thread를 만들어 그 결과를 출력하시오.
+ */
+public class JoinTest extends Thread{
+
+	int start, end, total;
+	
+	public JoinTest(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	@Override
+		public void run() {
+		int i;
+		for (i = start; i <= end; i++)
+			total += i;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Thread.currentThread() + " start");
+		
+		JoinTest jt1 = new JoinTest(1, 50);
+		JoinTest jt2 = new JoinTest(51, 100);
+		
+		jt1.start();
+		jt2.start();
+		
+		System.out.println("jt1.total = " + jt1.total);
+		System.out.println("jt2.total = " + jt1.total);
+		
+		int sum = jt1.total + jt2.total;
+		System.out.println("sum = " + sum);
+//		System.out.println();
+//		System.out.println(jt1.total + jt1.total);
+		
+		System.out.println(Thread.currentThread() + " end");
+		
+	}
+}
---
+++ Chap24(스레드)/src/kr/co/himedia/thread05/syncsolve02/SyncWaitNotifyTest03.java
@@ -0,0 +1,78 @@
+package kr.co.himedia.thread05.syncsolve02;
+
+import java.util.ArrayList;
+
+class HiMediaBookstore {
+	
+	public ArrayList<String> shelf = new ArrayList<>();
+	public HiMediaBookstore() {
+		shelf.add("인공지능 기반 자연어 처리 1");
+		shelf.add("인공지능 기반 자연어 처리 2");
+		shelf.add("인공지능 기반 자연어 처리 3");
+//		shelf.add("인공지능 기반 자연어 처리 4");
+//		shelf.add("인공지능 기반 자연어 처리 5");
+//		shelf.add("인공지능 기반 자연어 처리 6");
+	}
+	public synchronized String lendBook() throws InterruptedException {
+		
+		while(shelf.size() == 0) {
+			System.out.println(Thread.currentThread().getName() + " 빌리는 것 기다림 시작");
+			wait();
+			System.out.println(Thread.currentThread().getName() + " 빌리는 것 기다림 종료");
+		}
+		
+		String book = shelf.remove(0);
+		System.out.println(Thread.currentThread().getName() + " : " + book + " 빌림");
+		return book;
+	}
+	
+	public synchronized void retrunBook(String book) {
+		shelf.add(book);
+		notifyAll();
+		System.out.println(Thread.currentThread().getName() + " : " + book + " 반납함");
+	}
+}
+
+class Person extends Thread {
+	public Person(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			
+			String title = SyncWaitNotifyTest03.hmBookstore.lendBook();
+			if (title == null) return;
+			Thread.sleep(5000);
+			SyncWaitNotifyTest03.hmBookstore.retrunBook(title);
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+	}
+}
+
+public class SyncWaitNotifyTest03 {
+	
+	public static HiMediaBookstore hmBookstore = new HiMediaBookstore();
+	
+	public static void main(String[] args) {
+		
+		Person person1 = new Person("person1");
+		Person person2 = new Person("person2");
+		Person person3 = new Person("person3");
+		Person person4 = new Person("person4");
+		Person person5 = new Person("person5");
+		Person person6 = new Person("person6");
+		
+		person1.start();
+		person2.start();
+		person3.start();
+		person4.start();
+		person5.start();
+		person6.start();
+		
+	}
+
+}
